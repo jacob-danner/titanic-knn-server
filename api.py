a78dfc4a5cf35928ea2bf19a2681be37723db697
Jacob Danner <jacob.danner@drake.edu>
2022-09-29 12:28:14 -0500

    cors

--- api.py
+++ api.py
@@ -1,9 +1,19 @@
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from knn import make_knn
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Body(BaseModel):
     pass_class: int
